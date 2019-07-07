379e5a5474875507eecc15fbf46475e9b29c805e
Brian Weddell <8333056+BrianWeddell@users.noreply.github.com>
2019-07-07 15:22:11 -0500

    Update ssh_server.pp
    Added public key for master.

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDRGU1kL0rYeRNKL503uSKdzNrotMkhv3LmE2z/otJtYx+P614UloEQ4DRLzn7GIelMPlC1lyPZPjIRhdleCC9b5PfU2v15aSPxV6Wno32qPSNmhdilLv5yw/3dvDXfJTKU/FnSIrtmNT49QZznGrr9FZdgb6G5fZVlFse3d+P1ML2rR+mkwAC8wcfdymWupt45F4fYrZMO87ZnI98jmE8o//Ev5fRzBWYAT4Q0brcB09k4N5ftyJjxpqQCQHapI6VK45sZB8szxSgrKJ7i3E2KQ1+a3dnOMhKpXwCa0/RQR8WDx/OeF9T06HlPBzO0MI5attQ9H2uRxa29jgrpUonn',
 	}
 }
