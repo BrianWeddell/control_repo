a1da7df521d736c01c8fb182ef88b6fd487e0167
Brian Weddell <8333056+BrianWeddell@users.noreply.github.com>
2019-07-07 16:03:50 -0500

    Update agent_nodes.pp
    Added host resource type so that db and web are name reachable from docker nodes.

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,12 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.puppetagent.vm':}
   dockeragent::node {'db.puppetagent.vm':}
+  host {'web.puppetagent.vm':
+    ensure   => present,
+    ip       => '172.16.0.2',
+  }
+  host {'db.puppetagent.vm':
+    ensure   => present,
+    ip       => '172.16.0.3',
+  }
 }
