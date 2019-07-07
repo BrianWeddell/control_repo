4bf1b67140882f145cb93d13dc26714de728291f
Brian Weddell <8333056+BrianWeddell@users.noreply.github.com>
2019-07-07 16:43:03 -0500

    Update agent_nodes.pp
    added minecraft test docker container (minetest).

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,6 +2,7 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.puppetagent.vm':}
   dockeragent::node {'db.puppetagent.vm':}
+  dockeragent::node {'minetest.puppetagent.vm':}
   host {'web.puppetagent.vm':
     ensure   => present,
     ip       => '172.18.0.2',
